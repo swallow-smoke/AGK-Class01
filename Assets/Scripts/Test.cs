c3c9f2ec32c13ea32edb8b0554142a10b5624e37
swallow-smoke <teatimesucream@gmail.com>
2026-05-07 19:57:49 +0900

    Initial Commit

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("스페이스 눌림!!!!!");
+        }
+    }
+}
